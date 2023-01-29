8dbe2c94f30e324297eed59712fe45b0db1a8eda
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-29 15:55:38 +0000

    added selectors for span elements

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,12 @@ var cityName = "Birmingham";
 var numberofDays = 40;
 var searchButton = $('#search-button');
 var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + APIKey + '&cnt=' + numberofDays;
+var date = $('#date');
+var temp = $('#temp');
+var wind = $('#wind');
+var humidity = $('#humidity');
+
+// Create var with moment js to update time 
 
 function getWeatherForecast() {
     $.ajax({
@@ -10,6 +16,9 @@ function getWeatherForecast() {
         method: "GET"
     }).then(function (response) {
         console.log(response)
+        // Temparature for 18:00 
+        // Declare variable within scope
+        var temp = $('#temp').text(response.list[0].main.temp)
     });
 }
 getWeatherForecast();
