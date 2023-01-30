bb8b092385adb7e3d7983169df4cc96427430356
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-30 18:39:46 +0000

    update card content with AJAX objects

--- assets/script.js
+++ assets/script.js
@@ -26,14 +26,62 @@ $(searchButton).on('click', function (e) {
         console.log(response)
         // List of objects within JSON response
         for (var i = 0; i < response.list.length; i++) {
-            var dateAndTime = response.list[i].dt_txt;
+            var dateplusTime = response.list[i].dt_txt;
             // Extracts next 5 days from object using index
-            var year = dateAndTime.slice(0, 4);
-            var month = dateAndTime.slice(5, 7);
-            var day = dateAndTime.slice(8, 10);
+            var year = dateplusTime.slice(0, 4);
+            var month = dateplusTime.slice(5, 7);
+            var day = dateplusTime.slice(8, 10);
             // To format date
             var date = day + "/" + month + "/" + year;
-            console.log("date:" + date);
+            // To append weather icons to bootstrap cards
+            var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png"
+            // To get icon code of first element
+            var iconCode = response.list[0].weather[0].id
+            var weatherIcon = $('.weather-icon').attr('src', iconURL)
+            console.log(response.list[0].weather[0].id)
+            
+            // To render the search city to card title
+            var jumbotronCity = $('.card-city').text(" " + response.city.name)
+            // To access the first index [0]
+            var jumbotronTemp = $('#temp').text(" " + response.list[0].main.temp)
+            var jumbotronWind = $('#wind').text(" " + response.list[0].wind.speed)
+            var jumbotronHumidity = $('#humidity').text(" " + response.list[0].main.humidity)
+            var searchHistory = $('#search-history')
+            
+            // Displays cities searched to buttons + adds bootstrap class color of blue
+            var buttonHistory = $('<button>').text(response.city.name).addClass('btn-primary')
+            $('#search-history').append(buttonHistory);
+            
+            // Card content 1
+            var cardTime = $('#card1-time').text(" " + response.list[0].dt_txt)
+            var cardTemp = $('#card1-temp').text(" " + response.list[0].main.temp)
+            var cardWind = $('#card1-wind').text(" " + response.list[0].wind.speed)
+            var cardHumidity = $('#card1-humidity').text(" " + response.list[0].main.humidity)
+
+            // card content 2
+            var cardTime = $('#card2-time').text(" " + response.list[10].dt_txt)
+            var cardTemp = $('#card2-temp').text(" " + response.list[10].main.temp)
+            var cardWind = $('#card2-wind').text(" " + response.list[10].wind.speed)
+            var cardHumidity = $('#card2-humidity').text(" " + response.list[10].main.humidity)
+
+            // card content 3
+            var cardTime = $('#card3-time').text(" " + response.list[20].dt_txt)
+            var cardTemp = $('#card3-temp').text(" " + response.list[20].main.temp)
+            var cardWind = $('#card3-wind').text(" " + response.list[20].wind.speed)
+            var cardHumidity = $('#card3-humidity').text(" " + response.list[20].main.humidity)
+
+            // card content 4
+            var cardTime = $('#card4-time').text(" " + response.list[30].dt_txt)
+            var cardTemp = $('#card4-temp').text(" " + response.list[30].main.temp)
+            var cardWind = $('#card4-wind').text(" " + response.list[30].wind.speed)
+            var cardHumidity = $('#card4-humidity').text(" " + response.list[30].main.humidity)
+
+            // card content 5
+            var cardTime = $('#card5-time').text(" " + response.list[39].dt_txt)
+            var cardTemp = $('#card5-temp').text(" " + response.list[39].main.temp)
+            var cardWind = $('#card5-wind').text(" " + response.list[39].wind.speed)
+            var cardHumidity = $('#card5-humidity').text(" " + response.list[39].main.humidity)
+            
         }
         // Declare variable within scope
     });
